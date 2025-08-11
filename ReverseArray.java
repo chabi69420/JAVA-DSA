fbd2448123ca9a138777de676969e4bbdbfde480
Chabi chakraborty <149655837+chabi69420@users.noreply.github.com>
2025-08-11 11:41:09 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void reverse(int[] arr) {
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        reverse(arr);
+
+        System.out.print("Reversed array: ");
+        for (int num : arr){ 
+            System.out.print(num + " ");
+        }
+        sc.close();
+    }
+}
