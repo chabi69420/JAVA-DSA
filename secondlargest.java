18d023a04f8b76ba57d716ec5601f111c1a110e5
Chabi chakraborty <149655837+chabi69420@users.noreply.github.com>
2025-08-23 00:29:46 +0530

    Add files via upload

---
+++ secondlargest.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class secondlargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                second = largest;
+                largest = num;
+            } else if (num > second && num != largest) {
+                second = num;
+            }
+        }
+
+        if (second == Integer.MIN_VALUE)
+            System.out.println("No second largest element");
+        else
+            System.out.println("Second Largest = " + second);
+    }
+}
