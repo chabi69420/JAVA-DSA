9894204ecd69dbc6f98a2c0060ed65203b2c98ee
Chabi chakraborty <149655837+chabi69420@users.noreply.github.com>
2025-08-23 00:39:28 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String str = sc.next();
+
+        
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed = reversed + str.charAt(i);
+        }
+
+        if (str.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
