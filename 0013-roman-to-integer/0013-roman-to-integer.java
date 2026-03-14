857d34a25bd93fb9dd0917cb19ac7eaed1f92ea7
Chabi chakraborty <149655837+chabi69420@users.noreply.github.com>
2026-03-14 23:34:32 +0530

    Time: 5 ms (35.88%), Space: 46.6 MB (48.61%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int romanToInt(String s) {
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        int result = 0;
+
+        for(int i=0;i<s.length();i++)
+        {
+            int curr = map.get(s.charAt(i));
+
+            if(i < s.length()-1 && curr < map.get(s.charAt(i+1)))
+                result -= curr;
+            else
+                result += curr;
+        }
+
+        return result;
+    }
+}
+//Normally we add values.If a smaller value appears before a larger value → subtract
+//if current value < next value subtract else add.      TC=O(n)
